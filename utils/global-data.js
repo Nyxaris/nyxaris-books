5021ff60051666ad313f7190be10e0f40805f91d
Nyxaris <85733843+Nyxaris@users.noreply.github.com>
2023-11-19 06:42:20 -0500

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Nyxaris Books';
+    : 'Автор - Nyxaris';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Захватывающие истории';
+    : 'Приключения Эдмунда';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'Все права защищены.';
