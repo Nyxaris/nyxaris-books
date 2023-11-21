2e2d2e73cbbab3a3336a134533258d29921a9c74
Nyxaris <85733843+Nyxaris@users.noreply.github.com>
2023-11-21 10:09:51 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,8 @@ import SEO from '../components/SEO';
 export default function Index({ posts, globalData }) {
   return (
     <Layout>
-      <SEO title={globalData.name} description={globalData.blogTitle} />
-      <Header name={globalData.siteName} />
+      <SEO title={globalData.siteName} description={globalData.blogTitle} />
+      <Header name={globalData.name} />
       <main className="w-full">
         <h1 className="text-3xl lg:text-5xl text-center mb-12">
           {globalData.blogTitle}
